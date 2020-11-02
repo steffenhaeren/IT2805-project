12d5aef19740052a6998a45889fc98625217f65a
Gerhard Gustavsen <gerhard.gustavsen@outlook.com>
2020-11-02 14:38:11 +0100

    side på nett

---
+++ js/medlem.js
@@ -0,0 +1,14 @@
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+if(dd<10){
+        dd='0'+dd;
+    } 
+    if(mm<10){
+        mm='0'+mm;
+    } 
+
+today = (yyyy-13)+'-'+mm+'-'+dd; // max er i dag for 13 år siden! (man må være 13 for å bli medlem)
+document.getElementById("datefield").setAttribute("max", today);
+
---
+++ js/sok.js
@@ -0,0 +1,33 @@
+console.log("søk funksjon lastet inn");
+//funksjon for å ha samme text til samme ord:
+let last = "";
+let verdi = "";
+//søkefunksjon:
+const sok = () => {
+  last = verdi;
+  verdi = document.getElementById("sokemotor").value;
+  if (document.getElementById("sokemotor").value && verdi != last) {
+    let text = "";
+    const lorem =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    const ord = lorem.split(" ");
+    const random = Math.floor(Math.random() * ord.length);
+
+    let i = 0;
+
+    while (i <= random + 200) {
+      text = text + " " + ord[Math.floor(Math.random() * ord.length)];
+      i = i + 1;
+    }
+
+    document.getElementById("outputt").innerText = verdi + ":" + text;
+  }
+};
+//få enterfunskjonen til å funke
+document
+  .getElementById("sokemotor")
+  .addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+      sok();
+    }
+  });
